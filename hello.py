cc730cca77c73f39952fb175c1466af8e827262d
klllh <lh@kll.pl>
2018-11-06 16:04:22 +0100

    Hello

---
+++ hello.py
@@ -0,0 +1,15 @@
+import qiskit
+
+qr = qiskit.QuantumRegister(1)
+cr = qiskit.ClassicalRegister(1)
+program = qiskit.QuantumCircuit(qr, cr)
+program.measure(qr, cr)
+job = qiskit.execute(program, qiskit.Aer.get_backend('qasm_simulator'))
+print(job.result().get_counts())
+
+
+# qiskit.IBMQ.load_accounts()
+# backend = qiskit.backends.ibmq.least_busy(qiskit.IBMQ.backends(simulator=False))
+# print("We'll use the least busy device:",backend.name())
+# job = qiskit.execute( program, backend )
+# print( job.result().get_counts() )
