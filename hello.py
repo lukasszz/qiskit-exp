46c4130f6987dd2901fa4f54d15229c52317d8e6
lukasz <lukasz>
2018-11-08 12:50:30 +0100

    Run on a real machine

--- hello.py
+++ hello.py
@@ -7,9 +7,13 @@ program.measure(qr, cr)
 job = qiskit.execute(program, qiskit.Aer.get_backend('qasm_simulator'))
 print(job.result().get_counts())
 
+# Frist register token in python console
+# from qiskit import IBMQ
+# IBMQ.save_account('from: https://quantumexperience.ng.bluemix.net/qx/account/advanced ')
 
-# qiskit.IBMQ.load_accounts()
-# backend = qiskit.backends.ibmq.least_busy(qiskit.IBMQ.backends(simulator=False))
-# print("We'll use the least busy device:",backend.name())
-# job = qiskit.execute( program, backend )
-# print( job.result().get_counts() )
+
+qiskit.IBMQ.load_accounts()
+backend = qiskit.backends.ibmq.least_busy(qiskit.IBMQ.backends(simulator=False))
+print("We'll use the least busy device:", backend.name())
+job = qiskit.execute(program, backend)
+print(job.result().get_counts())
