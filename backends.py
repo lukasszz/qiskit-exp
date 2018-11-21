a154eed390dd239cfb84e034845b2b60b874fea3
lukasz <lukasz>
2018-11-21 22:16:48 +0100

    save backends status to file

--- backends.py
+++ backends.py
@@ -2,14 +2,16 @@ import pickle
 from datetime import datetime
 
 import qiskit
+from qiskit.backends.ibmq import IBMQBackend
 
 qiskit.IBMQ.load_accounts()
 backends = qiskit.IBMQ.backends()
 backends_status = []
+b : IBMQBackend
 for b in backends:
     print(b.name() + " status: ", end='')
     print(b.status())
-    backends_status.append(b.status())
+    backends_status.append(b.status().data)
 
 # Dump to file
 data = {'tms': datetime.utcnow(), 'backends': backends_status}
