088c13def89713348fc05a0fd778d410d8fac4c0
lukasz <lukasz.herok@gmail.com>
2019-04-10 07:29:50 +0200

    H_2 Hamilton integral with  1.43 UnitsType.BOHR = 0.7414 A

---
+++ h2_lh/h2_e05_pyscf_h2_intergrals.py
@@ -0,0 +1,12 @@
+from qiskit_chemistry.drivers import PySCFDriver, UnitsType
+
+'''
+H_2 Hamilton integral with  1.43 UnitsType.BOHR = 0.7414 A
+'''
+driver = PySCFDriver(atom='H 0.0 0.0 0.0; H 0.0 0.0 1.43', unit=UnitsType.BOHR,
+                     charge=0, spin=0, basis='sto3g')
+molecule = driver.run()
+
+print(molecule.one_body_integrals)
+print(molecule.two_body_integrals)
+
