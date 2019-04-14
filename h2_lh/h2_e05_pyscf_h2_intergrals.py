7cbd4c2a748afa0a4f84358d2d238d688a72af7f
lukasz <lukasz.herok@gmail.com>
2019-04-14 22:04:35 +0200

    Precision in distance

--- h2_lh/h2_e05_pyscf_h2_intergrals.py
+++ h2_lh/h2_e05_pyscf_h2_intergrals.py
@@ -3,7 +3,7 @@ from qiskit_chemistry.drivers import PySCFDriver, UnitsType
 '''
 H_2 Hamilton integral with  1.43 UnitsType.BOHR = 0.7414 A
 '''
-driver = PySCFDriver(atom='H 0.0 0.0 0.0; H 0.0 0.0 1.43', unit=UnitsType.BOHR,
+driver = PySCFDriver(atom='H 0.0 0.0 0.0; H 0.0 0.0 1.41968', unit=UnitsType.BOHR,
                      charge=0, spin=0, basis='sto3g')
 molecule = driver.run()
 
