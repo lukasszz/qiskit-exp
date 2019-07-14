5dd140aaa7520d4e92afd51fc3e3bd822749d4ac
lukasz <lukasz.herok@gmail.com>
2019-07-14 08:39:17 +0200

    Moved to qiskit 0.10

--- backends.py
+++ backends.py
@@ -2,24 +2,29 @@ import pickle
 from datetime import datetime
 
 import qiskit
-from qiskit.backends.ibmq import IBMQBackend
+from qiskit import IBMQ
+from qiskit.providers.models import BackendStatus
 
 qiskit.IBMQ.load_accounts()
-backends = qiskit.IBMQ.backends()
+# backends = Aer.backends()
+backends = IBMQ.backends()
+
 backends_status = []
-b : IBMQBackend
 for b in backends:
     print(b.name() + " status: ", end='')
     print(b.status())
-    backends_status.append(b.status().data)
+    st: BackendStatus = b.status()
+    backends_status.append({'pendging_jobs': st.pending_jobs,
+                            'name': st.backend_name,
+                            'operational': st.operational})
 
-# Dump to file
+print("Dump to file")
 data = {'tms': datetime.utcnow(), 'backends': backends_status}
 f = open('ibm_backends.pickle', 'wb')
 pickle.dump(data, f)
 f.close()
 
-# Load form file
+print("Load form file")
 f = open('ibm_backends.pickle', 'rb')
 dr = pickle.load(f)
 print(dr['tms'])
