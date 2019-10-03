88112a8a3ba5e90864291e8fd627e083be62b263
lukasszz <lukasz@lukaszherok.com>
2019-10-03 09:05:29 +0200

    test for two bits - half adder

--- ibm-challange/full-adder.py
+++ ibm-challange/full-adder.py
@@ -6,7 +6,7 @@ from qiskit import Aer, QuantumRegister, ClassicalRegister, QuantumCircuit, exec
 backend = Aer.get_backend('qasm_simulator')
 
 
-class FullAdder():
+class FullAdder:
 
     def __init__(self):
         self.q = QuantumRegister(7)
@@ -14,8 +14,10 @@ class FullAdder():
         self.circ = QuantumCircuit()
         self.circ.add_register(self.q)
         self.circ.add_register(self.c)
+        # s1c1 is the register for half adder summation - for the AB bits
         self.A, self.B, self.X, self.s1, self.c1, self.S, self.C = self.q
         self.result_state = []
+        self.result = {'ABX': '00', 's1c1': '00', 'SC': '00'}
 
     def build_circ(self):
         self.circ.cx(self.A, self.s1)
@@ -45,12 +47,15 @@ class FullAdder():
         if len(st) == 1:
             # self.result_state = list(st.keys())[0][::-1]
             self.result_state = list(st.keys())[0]
+            self.result['ABX'] = self.result_state[4:7][::-1]
+            self.result['s1c1'] = self.result_state[2:4]
+            self.result['SC'] = self.result_state[0:2]
         else:
             print("Unexpected output")
             print(st)
 
     def print_result_state(self):
-        print("Input XBA=%s, s1c1=%s result SC=%s " % (self.result_state[4:7], self.result_state[2:4], self.result_state[0:2]))
+        print("Input ABX=%s, s1c1=%s result SC=%s " % (self.result['ABX'], self.result['s1c1'], self.result['SC']))
 
 
 # print(circ)
@@ -64,6 +69,8 @@ if '__main__' == __name__:
     # print(fa.circ)
     fa.run()
     fa.print_result_state()
+    assert '01' == fa.result['s1c1']
+    assert '00' == fa.result['SC']
 
     fa = FullAdder()
     fa.input('110')
@@ -71,6 +78,7 @@ if '__main__' == __name__:
     # print(fa.circ)
     fa.run()
     fa.print_result_state()
+    assert '10' == fa.result['s1c1']
 
     fa = FullAdder()
     fa.input('000')
@@ -78,6 +86,7 @@ if '__main__' == __name__:
     # print(fa.circ)
     fa.run()
     fa.print_result_state()
+    assert '00' == fa.result['s1c1']
 
     fa = FullAdder()
     fa.input('010')
@@ -85,6 +94,7 @@ if '__main__' == __name__:
     # print(fa.circ)
     fa.run()
     fa.print_result_state()
+    assert '01' == fa.result['s1c1']
 
     fa = FullAdder()
     fa.input('001')
@@ -92,3 +102,4 @@ if '__main__' == __name__:
     # print(fa.circ)
     fa.run()
     fa.print_result_state()
+    assert '00' == fa.result['s1c1']
